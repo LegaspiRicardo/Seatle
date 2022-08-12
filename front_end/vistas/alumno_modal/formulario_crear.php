2699953efc56bb506caad5c82b1ff3abf27ec533
LegaspiRicardo <108900203+LegaspiRicardo@users.noreply.github.com>
2022-08-11 22:17:30 -0500

    Forms 4.3
    Working on "alumno" create form

--- front_end/vistas/alumno_modal/formulario_crear.php
+++ front_end/vistas/alumno_modal/formulario_crear.php
@@ -42,6 +42,30 @@ $("#exampleModal1").modal('show');
 }
 
 
+.img_subtitulo{
+  width: 6%;
+  display:inline-block;
+  
+  padding-bottom:2%;
+}
+
+h4{
+  display:inline;
+  padding-right:5%;
+}
+
+
+.styled-select select {
+   background: transparent;
+   border: none;
+}
+
+.borde_inferior{
+  border-top:solid 1px;
+  border-color:#c9c9c9;
+}
+
+
 </style>
 
 
@@ -52,74 +76,64 @@ $("#exampleModal1").modal('show');
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-       <h4 class="modal-title  ml-auto" id="exampleModalLabel">Registro Alumnos</h4>
+       <h3 class="modal-title  ml-auto" id="exampleModalLabel">Registro Alumnos</h3>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
 
     <div class="modal-body">
-      <form class="needs-validation" novalidate action="../../../back_end/controladores/alumno_controlador.php" method="POST" enctype="multipart/form-data" >
+      <form class="needs-validation " novalidate action="../../../back_end/controladores/alumno_controlador.php" method="POST" enctype="multipart/form-data" >
 
         <input type="hidden" name="opcion" value="1">
 
-
-
-        <!------------------------------------Nombres, Sexo------------------------------------------>
-        
-<h4>Información personal</h4>
-<div class="row mt-1">
-
-        <div class="col-4  mt-3">  
-          <img src="usuario_masculino.png" alt="" id="img_modal">
-
-          <input type="date" class="form-control mt-2 inputForm" id="validationCustom03" name="fecha_nac" required> 
- 
-
+        <!------------------------------------Img, Nombres, Sexo------------------------------------------>
+        <div class="Subtitulo">
+        <h4>Información personal</h4>
         </div>
-        
-        <div class="col-8 ml-auto pt-3">
-          <div class="form-row"> 
-            <div class="col">
-              <input type="text" class="form-control inputForm" id="validationCustom01" name="nombres" required>
-              <label for="validationCustom01">Nombre(s)</label>
-            </div>
 
-            <div class="col-3 ">
-            <select name="sexo" class="form-control" >
-                <option value="masculino" > Masculino</option>
-                <option value="femenino" >Femenino</option>
-
-            </select>
-              <label for="validationCustom02">Sexo</label>
-            </div>
-
+        <div class="row mt-1">
+          <div class="col-4  mt-3">  
+            <img src="usuario_masculino.png" alt="" id="img_modal">
+            <input type="date" class="form-control mt-2 inputForm" id="validationCustom03" name="fecha_nac" required> 
           </div>
+        
+          <div class="col-8 ml-auto pt-3">
+            <div class="form-row"> 
+              <div class="col">
+                <input type="text" class="form-control inputForm" id="validationCustom01" name="nombres" required>
+                <label for="validationCustom01">Nombre(s)</label>
+              </div>
 
-
-          <div class="form-row"> 
-            <div class="col">
-              <input type="text" class="form-control inputForm" id="validationCustom01" name="apellido_pat" required>
-              <label for="validationCustom01">Apellido Paterno</label>
+              <div class="col-3 ">
+                <select name="sexo" class="form-control" >
+                    <option value="masculino" > Masculino</option>
+                    <option value="femenino" >Femenino</option>
+                </select>
+                <label for="validationCustom02">Sexo</label>
+              </div>
             </div>
 
-            <div class="col">
-              <input type="text" class="form-control inputForm" id="validationCustom02" name="apellido_mat" required>
-              <label for="validationCustom02">Apellido Materno</label>
+            <div class="form-row"> 
+              <div class="col">
+                <input type="text" class="form-control inputForm" id="validationCustom01" name="apellido_pat" required>
+                <label for="validationCustom01">Apellido Paterno</label>
+              </div>
+
+              <div class="col">
+                <input type="text" class="form-control inputForm" id="validationCustom02" name="apellido_mat" required>
+                <label for="validationCustom02">Apellido Materno</label>
+              </div>
             </div>
           </div>
         </div>
-
-
-
-      </div>
-<br>
-<br>
-
-     <!------------------------------------Domicilio------------------------------------------>
-   
-<h4>Domicilio</h4>
-
+        <br>
+        <br>
+        <!------------------------------------Domicilio------------------------------------------>
+        <div class="Subtitulo ">
+        <h4 >Domicilio</h4>
+        <img src="ubicacion.png" class="img_subtitulo" alt="">
+        </div>
         <div class="form-row"> 
           <div class="col">
             <input type="text" class="form-control inputForm" id="validationCustom01" name="calle" required>
@@ -144,21 +158,67 @@ $("#exampleModal1").modal('show');
           </div>
         </div>
 
-        <br>
-    </div>
+       <br>
+       <br>
+        <!------------------------------------Domicilio------------------------------------------>
+        <div class="Subtitulo ">
+        <h4 >Alberca Info</h4>
+        <img src="tarjeta-de-identificacion.png" class="img_subtitulo" alt="">
+        </div>
+        <div class="form-row"> 
+          <div class="col styled-select black rounded ">
+                <select name="alberca" class="form-control" >
+                    <option value="Sta Tere" >Sta Tere</option>
+                    <option value="Zapopan" >Zapopan</option>
+                </select>
+            <label for="validationCustom01" class=" col borde_inferior" >Alberca</label>
+          </div>
 
-      <div class="modal-footer">
-        <input type="submit" class=" col-8 btn btn-primary mx-auto" value="Registrar Tutor">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
-      </div>
+          <div class="col">
+                <select name="tipo_clase" class="form-control" >
+                    <option value="Particular" >Particular </option>
+                    <option value="Grupal" >Grupal</option>
+                    <option value="Mixto" >Mixto</option>
+                </select><label for="validationCustom02">Tipo de clase</label>
+          </div>
+        </div>
+
+        <div class="form-row"> 
+          <div class="col">
+            <input type="text" class="form-control inputForm" id="validationCustom03" name="nivel" >
+            <label for="validationCustom03">Nivel</label>
+          </div>
+          
+          <div class="col">
+            <input type="text" class="form-control inputForm" id="validationCustom03" name="comentarios">
+            <label for="validationCustom03">Comentarios</label>
+          </div>
+        </div>
+
+        <br>
+        </div>
+
+
+
+
+
+        <div class="modal-footer">
+          <input type="submit" class=" col-8 btn btn-primary mx-auto" value="Registrar Alumno">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+        </div>
 
       </form>
+
+
+      
+
+
+
+
     </div>
   </div>
 </div>
-
-
 
 
 
