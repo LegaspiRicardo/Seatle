b5224173e94c55ab467947c3ee9ad2374fafbc55
LegaspiRicardo <108900203+LegaspiRicardo@users.noreply.github.com>
2022-12-08 13:00:17 -0600

    Aside update

--- front_end/vistas/privado/componentes/aside.php
+++ front_end/vistas/privado/componentes/aside.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
   <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar  collapse">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse">
       <div class="sidebar-sticky pt-3">
         <ul class="nav flex-column">
           <li class="nav-item ">
@@ -12,8 +12,7 @@
           </li>
           <li class=" col-12 pl-5 "><a class="nav-items " href="../dashboard/index.php"> Registrar Nuevo </a></li>
           <li class=" col-12 pl-5 "><a class="nav-items " href="../alumno/index.php"> Registrar Pago </a></li>
-          <li class=" col-12 pl-5 "><a class="nav-items " href="../alumno/index.php"> Actualizar Alumno </a></li>
-          <li class="col-12 pl-5 mb-3"><a class="nav-items" href="../alumno/index.php"> Dar de Baja </a></li>
+          <li class="col-12 pl-5 mb-3"><a class="nav-items" href="../alumno/index.php"> Ver ex alumnos </a></li>
 
           <li class="nav-item">
           <h5><a class="nav-link" href="../profesor/index.php"><img src="../../../PNGs/silbar.png" alt="" class="icon-sideBar mr-3">Profesores  </a></h5>
@@ -31,6 +30,12 @@
           <li class="nav-item">
             <h5><a class="nav-link" href="../mensualidad/index.php"><img src="../../../PNGs/monedas.png" alt="" class="icon-sideBar mr-3"> Pagos</a></h5>
           </li>
+
+          <li class="nav-item mt-5">
+            <h5><a class="nav-link " href="../../../../back_end/controladores/profesor_controlador.php?opcion=5" >  <img src="../../../PNGs/logout.png" alt="" class="icon-sideBar mr-3"> Cerrar sesión</a></h5>
+          </li>
+
+
 
 
 
