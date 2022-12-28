f881e308f29fced078ef78ce30d7bd0f855a2d1a
LegaspiRicardo <108900203+LegaspiRicardo@users.noreply.github.com>
2022-12-28 16:20:13 -0600

    Controladores Update

--- back_end/controladores/alumno_controlador.php
+++ back_end/controladores/alumno_controlador.php
@@ -28,7 +28,7 @@ if(isset($_REQUEST['opcion'])){
             break;
 
 
-        case '2':                                           //Actualizar alumno
+        case '2':                                           //Actualizar alumno en Index
             $alumno=new Alumno();
             $alumno->nombres=$_REQUEST['nombres'];
             $alumno->apellido_pat=$_REQUEST['apellido_pat'];
@@ -45,12 +45,34 @@ if(isset($_REQUEST['opcion'])){
             $alumno->comentarios=$_REQUEST['comentarios'];
             $alumno->alberca=$_REQUEST['alberca'];
             $alumno->status=$_REQUEST['status'];
-
             $alumno->id_alumno=$_REQUEST['id_alumno'];
             $resultado='';
             echo $alumno->actualizar();
             header('Location: ../../front_end/vistas/privado/alumno/index.php?resultado='.$resultado);
             break;  
+
+        case '5':                                           //Actualizar alumno en Index_baja
+            $alumno=new Alumno();
+            $alumno->nombres=$_REQUEST['nombres'];
+            $alumno->apellido_pat=$_REQUEST['apellido_pat'];
+            $alumno->apellido_mat=$_REQUEST['apellido_mat'];
+            $alumno->calle=$_REQUEST['calle'];
+            $alumno->numero=$_REQUEST['numero'];
+            $alumno->colonia=$_REQUEST['colonia'];
+            $alumno->cp=$_REQUEST['cp'];
+            $alumno->fecha_nac=$_REQUEST['fecha_nac'];
+            $alumno->sexo=$_REQUEST['sexo'];
+            $alumno->fecha_ing=$_REQUEST['fecha_ing'];
+            $alumno->id_tutor=$_REQUEST['id_tutor'];
+            $alumno->nivel=$_REQUEST['nivel'];
+            $alumno->comentarios=$_REQUEST['comentarios'];
+            $alumno->alberca=$_REQUEST['alberca'];
+            $alumno->status=$_REQUEST['status'];
+            $alumno->id_alumno=$_REQUEST['id_alumno'];
+            $resultado='';
+            echo $alumno->actualizar();
+            header('Location: ../../front_end/vistas/privado/alumno/index_baja.php?resultado='.$resultado);
+            break; 
             
 
         case '3':                                               //Eliminar alumno
@@ -59,32 +81,31 @@ if(isset($_REQUEST['opcion'])){
             $resultado='';
             echo $alumno->borrar();
             header('Location: ../../front_end/vistas/privado/alumno/index.php?resultado='.$resultado);
-            break;
+        break;
             default: echo"opcion invalida";
 
-            case '4':                                           //Actualizar alumno
-                $alumno=new Alumno();
-                $alumno->nombres=$_REQUEST['nombres'];
-                $alumno->apellido_pat=$_REQUEST['apellido_pat'];
-                $alumno->apellido_mat=$_REQUEST['apellido_mat'];
-                $alumno->calle=$_REQUEST['calle'];
-                $alumno->numero=$_REQUEST['numero'];
-                $alumno->colonia=$_REQUEST['colonia'];
-                $alumno->cp=$_REQUEST['cp'];
-                $alumno->fecha_nac=$_REQUEST['fecha_nac'];
-                $alumno->sexo=$_REQUEST['sexo'];
-                $alumno->fecha_ing=$_REQUEST['fecha_ing'];
-                $alumno->id_tutor=$_REQUEST['id_tutor'];
-                $alumno->nivel=$_REQUEST['nivel'];
-                $alumno->comentarios=$_REQUEST['comentarios'];
-                $alumno->alberca=$_REQUEST['alberca'];
-                $alumno->status=$_REQUEST['status'];
-    
-                $alumno->id_alumno=$_REQUEST['id_alumno'];
-                $resultado='';
-                echo $alumno->baja();
-                header('Location: ../../front_end/vistas/privado/alumno/index.php?resultado='.$resultado);
-                break; 
+        case '4':                                           //Dar de baja alumno
+            $alumno=new Alumno();
+            $alumno->nombres=$_REQUEST['nombres'];
+            $alumno->apellido_pat=$_REQUEST['apellido_pat'];
+            $alumno->apellido_mat=$_REQUEST['apellido_mat'];
+            $alumno->calle=$_REQUEST['calle'];
+            $alumno->numero=$_REQUEST['numero'];
+            $alumno->colonia=$_REQUEST['colonia'];
+            $alumno->cp=$_REQUEST['cp'];
+            $alumno->fecha_nac=$_REQUEST['fecha_nac'];
+            $alumno->sexo=$_REQUEST['sexo'];
+            $alumno->fecha_ing=$_REQUEST['fecha_ing'];
+            $alumno->id_tutor=$_REQUEST['id_tutor'];
+            $alumno->nivel=$_REQUEST['nivel'];
+            $alumno->comentarios=$_REQUEST['comentarios'];
+            $alumno->alberca=$_REQUEST['alberca'];
+            $alumno->status=$_REQUEST['status'];
+            $alumno->id_alumno=$_REQUEST['id_alumno'];
+            $resultado='';
+            echo $alumno->baja();
+            header('Location: ../../front_end/vistas/privado/alumno/index.php?resultado='.$resultado);
+        break; 
     }
 
 
--- back_end/controladores/clase_part_controlador.php
+++ back_end/controladores/clase_part_controlador.php
@@ -1,10 +1,7 @@
 <?php
 
 include_once '../modelos/clase_part.php';
-/*if(isset($_REQUEST)){
-    print_r($_REQUEST);
-}
-*/
+
 
 if(isset($_REQUEST['opcion'])){
     $opcion=$_REQUEST['opcion'];
@@ -12,34 +9,67 @@ if(isset($_REQUEST['opcion'])){
         case '1':      //crear
             $clase_part=new Clase_part();
             $clase_part->id_alumno=$_REQUEST['id_alumno'];
+            $clase_part->id_profesor=$_REQUEST['id_profesor'];
+            $clase_part->inicio=$_REQUEST['inicio'];
+            $clase_part->fin=$_REQUEST['fin'];
+            $clase_part->color=$_REQUEST['color'];
             $resultado='';
             echo $clase_part->crear();
+            header('Location: ../../front_end/vistas/privado/clase_part/index.php?resultado='.$resultado);
+        break;
 
-
-          //  header('Location:../../');
-
-            break;
-
-            case '2': 
+        case '5':      //crear clase en dashboard
             $clase_part=new Clase_part();
             $clase_part->id_alumno=$_REQUEST['id_alumno'];
+            $clase_part->id_profesor=$_REQUEST['id_profesor'];
+            $clase_part->inicio=$_REQUEST['inicio'];
+            $clase_part->fin=$_REQUEST['fin'];
+            $clase_part->color=$_REQUEST['color'];
+            $resultado='';
+            echo $clase_part->crear();
+            header('Location: ../../front_end/vistas/privado/dashboard/index.php?resultado='.$resultado);
+        break;
+
+        case '2':               //Actualizar
+            $clase_part=new Clase_part();
+            $clase_part->id_alumno=$_REQUEST['id_alumno'];
+            $clase_part->id_profesor=$_REQUEST['id_profesor'];
+            $clase_part->inicio=$_REQUEST['inicio'];
+            $clase_part->fin=$_REQUEST['fin'];
+            $clase_part->color=$_REQUEST['color'];
             $clase_part->id_clase=$_REQUEST['id_clase'];
             $resultado='';
             echo $clase_part->actualizar();
-                break;  //Actualizar
+        break;  
             
 
-            case '3':
+        case '3':
             $clase_part=new Clase_part();
             $clase_part->id_clase=$_REQUEST['id_clase'];
             $resultado='';
-            echo $clase_part->borrar();
-                
-                break;
-            default: echo"opcion invalida";
+            echo $clase_part->borrar();        
+        break;
+
+
+
+
+        case '4':
+            $clase_part=new Clase_part();
+            $clase_part->id_profesor=$_REQUEST['id_profesor'];
+            echo $clase_part->eventos(); 
+        
+            header('Location: ../../front_end/vistas/privado/clase_part/eventos.php?resultado='.$resultado); 
+            
+        break;
+
+
+
+
+
+
+
+    default: echo"opcion invalida";
     }
 
-    header('Location: ../../front_end/vistas/dashboard/index.php?resultado='.$resultado);
-    exit(); 
 
 }
